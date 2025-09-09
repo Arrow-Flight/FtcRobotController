ec8e1a34d764050956f05176405633eaf4984a76
Arrow-Flight <straffordrobotics11678@gmail.com>
2025-09-09 15:19:25 -0500

    Create VariablePractice.java
    created simple class for coach pratt tutorial

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariablePractice.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class VariablePractice extends OpMode {
+
+    @Override
+    public void init() {
+        int teamNumber = 11678;
+        double motorSpeed = 0.75;
+        boolean clawClosed = true;
+        String name = "Tyler";
+
+        telemetry.addData("Team Number", teamNumber);
+        telemetry.addData("Motor Speed", motorSpeed);
+        telemetry.addData("Claw Closed", clawClosed);
+        telemetry.addData("Name", name);
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
