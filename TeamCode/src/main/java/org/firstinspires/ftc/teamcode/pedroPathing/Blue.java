004343c3d2301a7da4575406953ad3273cfa8699
Arrow-Flight <straffordrobotics11678@gmail.com>
2025-11-13 19:25:08 -0600

    Update Blue.java
    Created a new blue side autonomous that shoots three preloaded balls then intakes two more and shoots them.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Blue.java
@@ -8,18 +8,31 @@ import com.pedropathing.util.Timer;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 
-@Disabled
 @Autonomous
 public class Blue extends OpMode {
+
+    // Shooter PIDF
+    private final Double shooterP = 0.0004;
+    private final Double shooterF = 0.00042;
+    private final Integer shooterTargetVelocity = 1100;
 
     // Limelight
     private Limelight3A limelight;
     private Pose3D limelightPose;
+
+    private Servo servoCamera;
+
+    // Motors
+    private DcMotor intake;
+    private DcMotor shooterLeft;
+    private DcMotor shooterRight;
 
     // Follower and Timer
     private Follower follower;
@@ -28,18 +41,38 @@ public class Blue extends OpMode {
     // Define positions
     private final Pose preStart = new Pose(22.8, 128, Math.toRadians(-45));
     private final Pose preFinal = new Pose(29.8, 119, Math.toRadians(-45));
-    private final Pose shootPose = new Pose(48, 96, Math.toRadians(-45));
+    private final Pose shootPose = new Pose(48, 96, Math.toRadians(-48));
+    private final Pose firstSpikeInitial = new Pose(48, 83.5, Math.toRadians(180));
+    private final Pose firstSpikeFinal = new Pose(24,83.5, Math.toRadians(180));
 
     // Define Paths
     private Path preMove;
+    private Path shootToFirstSpike;
+    private Path firstSpike;
+    private Path shootFromFirstSpike;
 
     int pathState = 0;
 
     @Override
     public void init() {
-        Servo servoCamera = hardwareMap.get(Servo.class, "servoCamera");
+        // Set Up servoCamera
+        servoCamera = hardwareMap.get(Servo.class, "servoCamera");
         servoCamera.scaleRange(0.3, 1.0);
         servoCamera.setPosition(0.3);
+
+        //Set Up intake
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Set Up shooter motors
+        shooterLeft = hardwareMap.get(DcMotor.class, "shooterLeft");
+        shooterRight = hardwareMap.get(DcMotor.class, "shooterRight");
+        shooterLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        shooterRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        shooterLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        shooterRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        shooterRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Set Up Limelight
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
@@ -48,9 +81,20 @@ public class Blue extends OpMode {
         follower = Constants.createFollower(hardwareMap);
         follower.setStartingPose(preStart);
 
-        // Build a simple path between the two poses
+        // Build Paths
         preMove = new Path(new BezierLine(preStart, preFinal));
         preMove.setLinearHeadingInterpolation(preStart.getHeading(), preFinal.getHeading());
+
+        shootToFirstSpike = new Path(new BezierLine(shootPose, firstSpikeInitial));
+        shootToFirstSpike.setLinearHeadingInterpolation(shootPose.getHeading(),firstSpikeInitial.getHeading());
+
+        firstSpike = new Path(new BezierLine(firstSpikeInitial, firstSpikeFinal));
+        firstSpike.setConstantHeadingInterpolation(firstSpikeFinal.getHeading());
+
+        shootFromFirstSpike = new Path(new BezierLine(firstSpikeInitial, shootPose));
+        shootFromFirstSpike.setLinearHeadingInterpolation(firstSpikeFinal.getHeading(),shootPose.getHeading());
+
+
 
         pathTimer = new Timer();
 
@@ -66,10 +110,14 @@ public class Blue extends OpMode {
 
     @Override
     public void loop() {
+        double shooterTargetPower = ((shooterF * shooterTargetVelocity)+ (shooterP * (shooterTargetVelocity - ((DcMotorEx)shooterRight).getVelocity())));
+        telemetry.addData("Name", pathTimer.getElapsedTimeSeconds());
         telemetry.update();
         follower.update();
 
+
         // Step 1: Run the first move
+        int velocity = 1100;
         if (pathState == 0 && !follower.isBusy()) {
             follower.followPath(preMove);
             pathTimer.resetTimer(); // track how long the move runs
@@ -92,17 +140,15 @@ public class Blue extends OpMode {
                 double xInches = -10 - (limelightPose.getPosition().y * 39.37);
                 double yInches = 159.7 + (limelightPose.getPosition().x * 39.37);
                 double headingRadians = Math.toRadians(limelightPose.getOrientation().getYaw() - 90);
-                telemetry.addData("X", xInches);
-                telemetry.addData("Y", yInches);
-                telemetry.addData("Heading", headingRadians);
-/*
+
                 Pose startPose = new Pose(xInches, yInches, headingRadians);
                 moveToShoot = new Path(new BezierLine(startPose, shootPose));
                 moveToShoot.setLinearHeadingInterpolation(startPose.getHeading(), shootPose.getHeading());
 
                 follower.followPath(moveToShoot);
                 limelight.stop();
- */
+                servoCamera.setPosition(1.0);
+
                 pathState = 2;
             }
 
@@ -117,15 +163,54 @@ public class Blue extends OpMode {
 
                 follower.followPath(moveToShoot);
                 limelight.stop();
+                pathTimer.resetTimer();
                 pathState = 2;
             }
         }
 
-        // Step 3: After moving to shoot pose
+        // Step 3: Shoot Ball
         else if (pathState == 2 && !follower.isBusy()) {
-            telemetry.addLine("Arrived at shoot pose!");
-            telemetry.update();
-            // Ready for next action (like shooting)
+            shooterRight.setPower(shooterTargetPower);
+            shooterLeft.setPower(shooterTargetPower);
+
+            if (pathTimer.getElapsedTimeSeconds() >= 5.0) {
+                intake.setPower(0);
+                shooterRight.setPower(0);
+                shooterLeft.setPower(0);
+                pathState = 3;
+
+            } else if (pathTimer.getElapsedTimeSeconds() >= 2.0) {
+                intake.setPower(1);
+            }
+
+        } else if (pathState == 3 && !follower.isBusy()) {
+            follower.followPath(shootToFirstSpike);
+            pathState = 4;
+
+        } else if (pathState == 4 && !follower.isBusy()) {
+            intake.setPower(1);
+            follower.followPath(firstSpike);
+            pathState = 5;
+
+        } else if (pathState == 5 && !follower.isBusy()) {
+            intake.setPower(0);
+            follower.followPath(shootFromFirstSpike);
+            pathTimer.resetTimer();
+            pathState = 6;
+
+        } else if (pathState == 6 && !follower.isBusy()) {
+            shooterRight.setPower(shooterTargetPower);
+            shooterLeft.setPower(shooterTargetPower);
+
+            if (pathTimer.getElapsedTimeSeconds() >= 5.0) {
+                intake.setPower(0);
+                shooterRight.setPower(0);
+                shooterLeft.setPower(0);
+                pathState = 7;
+
+            } else if (pathTimer.getElapsedTimeSeconds() >= 2.0) {
+                intake.setPower(1);
+            }
         }
     }
 
